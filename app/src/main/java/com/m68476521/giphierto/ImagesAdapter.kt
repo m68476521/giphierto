1405fdefeaeee164e10ffe6e96af223c7b68903b
Mike Wasauskee <m68476521.1@gmail.com>
2020-06-23 11:12:00 -0700

    Add Glide dependency
    - Remove picasso lib and add Glide to see the ghift.
    
    by m68476521.1@gmail.com

--- app/src/main/java/com/m68476521/giphierto/ImagesAdapter.kt
+++ app/src/main/java/com/m68476521/giphierto/ImagesAdapter.kt
@@ -1,18 +1,21 @@
 package com.m68476521.giphierto
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.m68476521.giphierto.api.Image
-import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.image_item.view.*
 
 class ImagesAdapter : RecyclerView.Adapter<ImageHolder>() {
 
     private var imagesList: List<Image> = emptyList()
+    lateinit var context: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageHolder {
+        context = parent.context
         return ImageHolder(
             LayoutInflater.from(parent.context).inflate(R.layout.image_item, parent, false)
         )
@@ -21,7 +24,11 @@ class ImagesAdapter : RecyclerView.Adapter<ImageHolder>() {
     override fun getItemCount(): Int = imagesList.size
 
     override fun onBindViewHolder(holder: ImageHolder, position: Int) {
-        holder.bind(imagesList[position].images.fixedHeightSmall.url, imagesList[position].title)
+        holder.bind(
+            imagesList[position].images.fixedHeightSmall.url,
+            imagesList[position].title,
+            context
+        )
     }
 
     fun swapImages(images: List<Image>) {
@@ -34,10 +41,14 @@ class ImagesAdapter : RecyclerView.Adapter<ImageHolder>() {
 }
 
 class ImageHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-    fun bind(image: String, name: String) {
-        Picasso.get()
-            .load(image)
-            .into(itemView.imageUrl)
+    fun bind(image: String, name: String, context: Context) {
         itemView.title.text = name
+
+        Glide
+            .with(context)
+            .load(image)
+            .centerCrop()
+            .placeholder(R.drawable.ic_launcher_background)
+            .into(itemView.imageUrl)
     }
 }
