5d4f7ee26bfcc43ae7e27bfd4565bd7336dbff3c
Mike Wasauskee <m68476521.1@gmail.com>
2020-06-24 16:31:15 -0700

    Add navigation components.
    - Create fragment home and move the recycler view to the home fragment
    
    by m68476521.1@gmail.com

--- app/src/main/java/com/m68476521/giphierto/MainActivity.kt
+++ app/src/main/java/com/m68476521/giphierto/MainActivity.kt
@@ -9,32 +9,12 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val API_KEY = ""
-class MainActivity : AppCompatActivity() {
 
-    private var imagesAdapter = ImagesAdapter()
-    private val compositeDisposable = CompositeDisposable()
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         GiphyManager.setToken(API_KEY)
-        images.layoutManager = GridLayoutManager(this, 3)
-        images.adapter = imagesAdapter
-        search()
-    }
-
-    private fun search() {
-        val disposable = GiphyManager.giphyApi.trending()
-            .subscribeOn(Schedulers.io())
-            .subscribe({
-                runOnUiThread { imagesAdapter.swapImages(it.data) }
-            }, { it.printStackTrace() })
-        compositeDisposable.add(disposable)
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        compositeDisposable.dispose()
     }
 }
---
+++ app/src/main/java/com/m68476521/giphierto/home/Home.kt
@@ -0,0 +1,51 @@
+package com.m68476521.giphierto.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import com.m68476521.giphierto.ImagesAdapter
+import com.m68476521.giphierto.R
+import com.m68476521.giphierto.api.GiphyManager
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_home.*
+
+class Home : Fragment() {
+
+    private var imagesAdapter = ImagesAdapter()
+    private val compositeDisposable = CompositeDisposable()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        images.layoutManager = GridLayoutManager(requireContext(), 3)
+        images.adapter = imagesAdapter
+        search()
+    }
+
+    private fun search() {
+        println("MIKE")
+        val disposable = GiphyManager.giphyApi.search("dog")
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeOn(Schedulers.io())
+            .subscribe({
+                imagesAdapter.swapImages(it.data)
+            }, { it.printStackTrace() })
+        compositeDisposable.add(disposable)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        compositeDisposable.dispose()
+    }
+}
