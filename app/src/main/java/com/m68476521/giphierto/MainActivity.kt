ce0cd5228cdc6ecd1993d06f4930006fb4071c6e
Mike Wasauskee <m68476521.1@gmail.com>
2020-06-27 11:37:01 -0700

    Add Search Fragment
    - also add navigation bottom
    
    by m68476521.1@gmail.com

--- app/src/main/java/com/m68476521/giphierto/MainActivity.kt
+++ app/src/main/java/com/m68476521/giphierto/MainActivity.kt
@@ -2,10 +2,9 @@ package com.m68476521.giphierto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.GridLayoutManager
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.m68476521.giphierto.api.GiphyManager
-import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val API_KEY = ""
@@ -16,5 +15,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         GiphyManager.setToken(API_KEY)
+        setUpBottomNavigation()
+    }
+
+    private fun setUpBottomNavigation() {
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment?
+        if (navHostFragment != null)
+            NavigationUI.setupWithNavController(bottom_navigation, navHostFragment.navController)
     }
 }
