f9aade0a141a4b4f021cb41198ff49e6ea3ee86b
Mike Wasauskee <m68476521.1@gmail.com>
2020-07-09 22:07:43 -0700

    Change the navigation bottom
    - In this commit I change the navigation to a custom listener. This is for avoid
    to recreate the fragment once that It has been created.
    
    by m68476521.1@gmail.com

--- app/src/main/java/com/m68476521/giphierto/MainActivity.kt
+++ app/src/main/java/com/m68476521/giphierto/MainActivity.kt
@@ -2,37 +2,58 @@ package com.m68476521.giphierto
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.m68476521.giphierto.api.GiphyManager
+import com.m68476521.giphierto.home.Home
+import com.m68476521.giphierto.search.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 const val API_KEY = ""
 
 class MainActivity : AppCompatActivity() {
 
+    private val homeFragment = Home()
+    private val searchFragment = SearchFragment()
+    private var activeFragment: Fragment = homeFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         GiphyManager.setToken(API_KEY)
+
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.container, homeFragment, getString(R.string.app_name)).hide(homeFragment)
+            add(R.id.container, searchFragment, getString(R.string.home)).hide(searchFragment)
+
+        }.commit()
+
+        supportFragmentManager.beginTransaction().hide(activeFragment)
+            .show(homeFragment).commit()
+        activeFragment = homeFragment
         setUpBottomNavigation()
     }
 
     private fun setUpBottomNavigation() {
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.navHostFragment) as NavHostFragment?
-        if (navHostFragment != null)
-            NavigationUI.setupWithNavController(bottom_navigation, navHostFragment.navController)
-
-        setActionBarNav()
+        bottom_navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
     }
 
-    private fun setActionBarNav() {
-        val navController = findNavController(R.id.navHostFragment)
-        bottom_navigation.setupWithNavController(navController)
-        setupActionBarWithNavController(navController)
-    }
+    private val mOnNavigationItemSelectedListener =
+        BottomNavigationView.OnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.homeFragment -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment)
+                        .show(homeFragment).commit()
+                    activeFragment = homeFragment
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.searchFragment -> {
+                    supportFragmentManager.beginTransaction().hide(activeFragment)
+                        .show(searchFragment).commit()
+                    activeFragment = searchFragment
+                    return@OnNavigationItemSelectedListener true
+                }
+            }
+            false
+        }
 }
