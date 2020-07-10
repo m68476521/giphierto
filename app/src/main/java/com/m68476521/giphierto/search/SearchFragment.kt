07d77fe8429e3b080bbf53cb3af4abc839fb85b7
Mike Wasauskee <m68476521.1@gmail.com>
2020-07-09 22:23:56 -0700

    Hide keyboard input after search fron EditText
    by m68476521.1@gmail.com

--- app/src/main/java/com/m68476521/giphierto/search/SearchFragment.kt
+++ app/src/main/java/com/m68476521/giphierto/search/SearchFragment.kt
@@ -1,20 +1,22 @@
 package com.m68476521.giphierto.search
 
+import android.content.Context
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.TextView.OnEditorActionListener
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.m68476521.giphierto.ImagesAdapter
+import com.m68476521.giphierto.R
 import com.m68476521.giphierto.api.GiphyManager
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import com.m68476521.giphierto.R
 import kotlinx.android.synthetic.main.fragment_search.*
 
 class SearchFragment : Fragment() {
@@ -42,6 +44,9 @@ class SearchFragment : Fragment() {
                     event != null && event.action == KeyEvent.ACTION_DOWN && event.keyCode == KeyEvent.KEYCODE_ENTER
                 ) {
                     if (event == null || !event.isShiftPressed) {
+                        val imm: InputMethodManager =
+                            requireActivity().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.hideSoftInputFromWindow(searchEdit.windowToken, 0)
                         search(searchEdit.text.toString())
                         return@OnEditorActionListener true
                     }
