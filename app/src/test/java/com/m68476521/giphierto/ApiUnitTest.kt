45cbd372b5f8362abfb4b2b2e8a5435642e6fd89
Mike Wasauskee <m68476521.1@gmail.com>
2020-07-29 14:36:34 -0700

    Add Unit test for API
    - test categories end point.
    
    by m68476521.1@gmail.com

---
+++ app/src/test/java/com/m68476521/giphierto/ApiUnitTest.kt
@@ -0,0 +1,24 @@
+package com.m68476521.giphierto
+
+import com.m68476521.giphierto.api.GiphyManager
+import org.junit.Before
+import org.junit.Test
+
+private const val TOKEN = ""
+
+class ApiUnitTest {
+    @Before
+    fun setupApi() {
+        GiphyManager.setToken(TOKEN)
+    }
+
+    @Test
+    fun getCategories() {
+        GiphyManager.giphyApi.categories()
+            .test()
+            .assertNoErrors()
+            .assertValue {
+                it.data.isNotEmpty()
+            }
+    }
+}
