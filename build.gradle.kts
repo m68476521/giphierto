e0e522ba20821c215e15d07ba1d0179d58e710f0
Miguel Orozco <m68476521.1@gmail.com>
2026-01-19 16:53:09 -0800

    REFACTORr: Change project structure

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ plugins {
     alias(libs.plugins.dagger.hilt)  apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
+    alias(libs.plugins.android.library) apply false
 //    alias(libs.plugins.google.services) apply false
 //    alias(libs.plugins.crashlytics) apply false
 }
